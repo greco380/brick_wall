895c8a2ce78631d33e883bcd9085b4a9efcc7717
Greco380 <jdg1492@rit.edu>
2019-10-06 02:32:32 -0400

    Initial commit, More to come. This is just a little setup

---
+++ SQLAlchemy.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisIsATestSecretKey3'
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///site.db'
+
+posts = [
+    {
+        'student': 'Miles Morales',
+        'company': 'Hooli',
+        'salary': '$100,000',
+        'position': 'Database Engineer',
+        'date_employed': 'August 23, 2019'
+    }
+    {
+        'student': 'Cavin F.H.Y.Z.C.I.T.C.J.C.J.S.-M',
+        'company': 'Pied Piper',
+        'salary': '$120,000',
+        'position': 'Full Stack Scratch Developer',
+        'date_employed': 'April 20, 2020'
+    }
+
+]
